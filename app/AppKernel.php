d4abee8fd063eecaa4747ae22a7b27e241dc7473
Jean-Pierre Lambelet <jp_lambelet_@hotmail.com>
2016-03-14 21:56:42 +0100

    First homepage

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new AppBundle\AppBundle()
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/AppBundle/Controller/HomepageController.php
+++ src/AppBundle/Controller/HomepageController.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Jean-Pierre Lambelet (jlambelet@contentfleet.com)
+ * Date: 12/03/16
+ * Time: 16:10
+ */
 
 namespace AppBundle\Controller;
 
@@ -6,16 +12,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class HomepageController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('AppBundle:Homepage:homepage.html.twig');
     }
 }
