c929d053530595199d1b4546e8e85dc4b946eb4e
lambeletjp <jlambelet@contentfleet.com>
2016-09-17 12:54:15 +0200

    Correct script

--- web/js/script.js
+++ web/js/script.js
@@ -5,38 +5,40 @@ function initMap() {
     longitude = parseFloat(longitude);
     var coords = {'lat':latitude,'lng':longitude};
     var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-        center: coords,
-        zoom: 14
-    });
+    if(mapDiv) {
+        var map = new google.maps.Map(mapDiv, {
+            center: coords,
+            zoom: 14
+        });
 
-    var url = 'app_dev.php/api/places?lat='+coords.lat+'&lng='+coords.lng;
-    $.getJSON( url, function( data ) {
-        var items = [];
-        var infoWindowOpen = null;
-        $.each( data, function( key, place ) {
-            var coords = {'lat':place.latitude,'lng':place.longitude};
-            var marker = new google.maps.Marker({
-                position: coords,
-                map: map,
-                title: 'Click to zoom'
-            });
-
-            marker.addListener('click', function() {
-                var contentString = place.google_info_box;
-
-                var infowindow = new google.maps.InfoWindow({
-                    content: contentString
+        var url = 'app_dev.php/api/places?lat=' + coords.lat + '&lng=' + coords.lng;
+        $.getJSON(url, function (data) {
+            var items = [];
+            var infoWindowOpen = null;
+            $.each(data, function (key, place) {
+                var coords = {'lat': place.latitude, 'lng': place.longitude};
+                var marker = new google.maps.Marker({
+                    position: coords,
+                    map: map,
+                    title: 'Click to zoom'
                 });
 
-                if(infoWindowOpen){
-                    infoWindowOpen.close();
-                }
-                infowindow.open(map, marker);
-                infoWindowOpen = infowindow;
+                marker.addListener('click', function () {
+                    var contentString = place.google_info_box;
+
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString
+                    });
+
+                    if (infoWindowOpen) {
+                        infoWindowOpen.close();
+                    }
+                    infowindow.open(map, marker);
+                    infoWindowOpen = infowindow;
+                });
             });
         });
-    });
+    }
 }
 
 function initMapPlace() {
