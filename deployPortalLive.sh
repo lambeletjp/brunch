e1a5467f8ad38544212df3e8340ca27c25ac3ae3
lambeletjp <jlambelet@contentfleet.com>
2016-10-29 16:30:08 +0200

    add deployPortalLive

---
+++ deployPortalLive.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+/usr/bin/php5.5-cli composer.phar install
+/usr/bin/php5.5-cli bin/console cache:clear
+/usr/bin/php5.5-cli bin/console cache:clear -e prod
+/usr/bin/php5.5-cli bin/console assets:install
+/usr/bin/php5.5-cli bin/console fos:comment:installAces
+printf "parameters: \n    assets_version: `shuf -i 0-1000000 -n 1` \n    deploy_timestamp: 0" > app/config/version.yml
