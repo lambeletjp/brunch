e9ee1efd2ef63bf5aa2844e2abd46c34bc9a5b8c
lambeletjp <jlambelet@contentfleet.com>
2016-10-29 15:55:09 +0200

    Composer update

--- deployPortal.sh
+++ deployPortal.sh
@@ -4,3 +4,4 @@ php bin/console cache:clear
 php bin/console cache:clear -e prod
 php bin/console assets:install
 php bin/console fos:comment:installAces
+printf "parameters: \n    assets_version: `shuf -i 0-1000000 -n 1` \n    deploy_timestamp: 0" > app/config/version.yml
