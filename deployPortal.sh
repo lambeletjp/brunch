4515548b2b82ce0f2c789909153f99c4dcd31143
lambeletjp <jlambelet@contentfleet.com>
2016-11-11 19:55:36 +0100

    Login with Social possible

--- deployPortal.sh
+++ deployPortal.sh
@@ -5,4 +5,5 @@ php bin/console cache:clear
 php bin/console cache:clear -e prod
 php bin/console assets:install
 php bin/console fos:comment:installAces
+php bin/console doctrine:schema:update --force
 printf "parameters: \n    assets_version: `shuf -i 0-1000000 -n 1` \n    deploy_timestamp: 0" > app/config/version.yml
--- deployPortalLive.sh
+++ deployPortalLive.sh
@@ -5,4 +5,5 @@ git pull
 /usr/bin/php5.5-cli bin/console cache:clear -e prod
 /usr/bin/php5.5-cli bin/console assets:install
 /usr/bin/php5.5-cli bin/console fos:comment:installAces
+/usr/bin/php5.5-cli bin/console doctrine:schema:update --force
 printf "parameters: \n    assets_version: `shuf -i 0-1000000 -n 1` \n    deploy_timestamp: 0" > app/config/version.yml
