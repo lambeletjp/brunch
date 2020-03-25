5991c3644e4aa6b809396a8d24eed2b54037bb06
lambeletjp <jlambelet@contentfleet.com>
2020-03-25 15:29:35 +0100

    Working

--- portal/src/Entity/User.php
+++ portal/src/Entity/User.php
@@ -112,4 +112,9 @@ class User implements UserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    public function setEnabled($true)
+    {
+
+    }
 }
